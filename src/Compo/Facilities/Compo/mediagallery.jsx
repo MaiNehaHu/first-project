785e8b37607c6a312b3e3be477900f60fba4abe3
MaiNehaHu <neha207y1a0423@mlritm.ac.in>
2022-12-08 17:48:55 +0530

    gallery photos added

--- src/Compo/Facilities/Compo/mediagallery.jsx
+++ src/Compo/Facilities/Compo/mediagallery.jsx
@@ -19,16 +19,58 @@ const Mediagallery = () => {
               <img src="https://api.mlritm.ac.in//images/gallery/big/5b3a1566887de09-10-2017.jpg" />
             </div>
             <div className="pics">
-                <img src="https://api.mlritm.ac.in//images/gallery/big/5b3a1566b3f389.jpg" />
+              <img src="https://api.mlritm.ac.in//images/gallery/big/5b3a1566b3f389.jpg" />
             </div>
             <div className="pics">
               <img src="https://api.mlritm.ac.in//images/gallery/big/5b3a1566deec210.jpg" />
             </div>
             <div className="pics">
-              <img src="https://api.mlritm.ac.in//images/gallery/big/5b3a156995f65andhrajyothi.jpg"  />
+              <img src="https://api.mlritm.ac.in//images/gallery/big/5b3a156995f65andhrajyothi.jpg" />
             </div>
             <div className="pics">
               <img src="https://api.mlritm.ac.in//images/gallery/big/5b3a157cd47cascan0111.jpg" />
+            </div>
+            <div className="pics">
+              <img src="https://api.mlritm.ac.in//images/gallery/big/5d8830f11b44bWhatsApp%20Image%202019-09-23%20at%2007.59.45%20(7).jpeg" />
+            </div>
+            <div className="pics">
+              <img src="https://api.mlritm.ac.in//images/gallery/big/5d8830f1c8642WhatsApp%20Image%202019-09-23%20at%2007.59.45%20(1).jpeg" />
+            </div>
+            <div className="pics">
+              <img src="https://api.mlritm.ac.in//images/gallery/big/5d8830f16a18eWhatsApp%20Image%202019-09-23%20at%2007.59.45%20(4).jpeg" />
+            </div>
+            <div className="pics">
+              <img src="https://api.mlritm.ac.in//images/gallery/big/5cd84cf0b60b6FB_IMG_1556148996851" />
+            </div>
+            <div className="pics">
+              <img src="https://api.mlritm.ac.in//images/gallery/big/5cd84cf1078beFB_IMG_1556149024636" />
+            </div>
+            <div className="pics">
+              <img src="https://api.mlritm.ac.in//images/gallery/small/FB_IMG_1556149092496" />
+            </div>
+            <div className="pics">
+              <img src="https://api.mlritm.ac.in//images/gallery/big/5cd84ef31c132IMG-20190421-WA0027" />
+            </div>
+            <div className="pics">
+              <img src="https://api.mlritm.ac.in//images/gallery/big/5cd84ef33278dIMG-20190421-WA0028" />
+            </div>
+            <div className="pics">
+              <img src="https://api.mlritm.ac.in//images/gallery/big/5cd84e6d9b298IMG-20190421-WA0015" />
+            </div>
+            <div className="pics">
+              <img src="https://api.mlritm.ac.in//images/gallery/big/5cd84dfc07e2bFB_IMG_1556149070111" />
+            </div>
+            <div className="pics">
+              <img src="https://api.mlritm.ac.in//images/gallery/big/5cd84dfbd26c9FB_IMG_1556149054057" />
+            </div>
+            <div className="pics">
+              <img src="https://api.mlritm.ac.in//images/gallery/big/5bf3ba3857a03_DSC0151.JPG" />
+            </div>
+            <div className="pics">
+              <img src="https://api.mlritm.ac.in//images/gallery/big/5bfe9114c444bIMG_1425.JPG" />
+            </div>
+            <div className="pics">
+              <img src="https://api.mlritm.ac.in//images/gallery/big/5bfe91a740ad3IMG_1455.JPG" />
             </div>
           </div>
         </div>
